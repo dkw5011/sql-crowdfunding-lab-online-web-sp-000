09515923e71d68012096cef6a4fc1ceb75a1d043
Danielle Bergbower <dkw5011@gmail.com>
2020-04-02 03:06:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,6 +38,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT Projects.category, SUM(Pledges.amount)
   FROM Projects
   JOIN Pledges
+  On Project.id = Pledges.user_id
   "
 end
 
