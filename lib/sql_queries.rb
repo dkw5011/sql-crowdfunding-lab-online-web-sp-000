3807df704808192a97e1ebc058959f9690758de0
Danielle Bergbower <dkw5011@gmail.com>
2020-04-02 02:53:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   FROM Users
   JOIN Pledges
   ON Users.id = Pledges.user_id
-  ORDER BY SUM(Pledges.amount);"
+  GROUP BY SUM(Pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
