fc105b6d692cb8220b7bf1c3177ea1e4334a6b3d
Danielle Bergbower <dkw5011@gmail.com>
2020-04-02 03:12:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,4 +49,5 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   JOIN Pledges
   On Project.id = Pledges.project_id
   WHERE projects.category = "books";"
+  
 end
