29f52b55bedc74c3c8bfb6311a121e95d2955d28
Danielle Bergbower <dkw5011@gmail.com>
2020-04-02 03:28:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT Projects.category, Pledges.amount
   FROM Projects
   JOIN Pledges
-  On Projects.id = Pledges.user_id
+  On Projects.id = Pledges.project_id
   WHERE projects.category = 'music';"
   
 end
