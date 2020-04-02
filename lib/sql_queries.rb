2c14b802af80cdd95566614d6e2f26990eff0e41
Danielle Bergbower <dkw5011@gmail.com>
2020-04-02 03:25:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT Projects.category, SUM(Pledges.amount)
+  "SELECT Projects.category, Pledges.amount
   FROM Projects
   JOIN Pledges
   On Projects.id = Pledges.user_id
